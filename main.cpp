783732cba1039ba85b6ef6d0c22cb86a8953845b
Yanggan <376139949@qq.com>
2025-03-17 23:22:13 +0800

    练习构造函数和析构函数

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
-#include "Log.h"
+
+class Entity {
+public:
+  float x,y;
+  //构造函数
+  Entity() {
+    x = 0.0f;y = 0.0f;
+    std::cout << "Created Entity" << std::endl;
+  }
+  //构造函数重载
+  Entity(float a, float b) {
+    x = a;y = b;
+  }
+  //Destructor析构函数
+  ~Entity() {
+    std::cout << "Destroyed Entity" << std::endl;
+  }
+
+  void Print() const {
+    std::cout << x <<","<< y << std::endl;
+  }
+};
+
+void Function() {
+  Entity e;
+  e.Print();
+}
 
 int main() {
   std::cout << "main!!!" <<std::endl;
-  return 0 ;
+  Function();
+  std::cin.get();
+  return 0;
 }
