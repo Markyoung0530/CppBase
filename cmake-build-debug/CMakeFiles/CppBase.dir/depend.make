e837db486b9d7dabfd9a482dc68e8f5a50c57bdb
markyoungWindows <markyoung0530@outlook.com>
2025-03-17 17:30:48 +0800

    2nd Commit

---
+++ cmake-build-debug/CMakeFiles/CppBase.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CppBase.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,51 +1,47 @@
 #include <iostream>
-#include "Log.h"
-// class Log {
-// public:
-//     const static int LogLevelError = 0;
-//     const static int LogLevelWarning = 1;
-//     const static int LogLevelInfo = 2;
-// private:
-//     int m_LogLevel = LogLevelInfo;
-// public:
-//     void SetLevel(int level)
-//     {
-//         m_LogLevel = level;
-//     }
-//     void Warn(const char* msg) const {
-//         if (m_LogLevel >= LogLevelWarning){
-//             std::cout <<"[WARNING]: " << msg << std::endl;
-//         }
-//     }
-//     void Error(const char* msg) const {
-//         if (m_LogLevel >= LogLevelError) {
-//             std::cout <<"[ERROR]: " << msg << std::endl;
-//         }
-//     }
-//     void Info(const char* msg) const
-//     {
-//         if (m_LogLevel >=LogLevelInfo) {
-//             std::cout <<"[INFO]: " << msg << std::endl;
-//         }
-//     }
-// };
+// #include "Log.h"
+class Log {
+public:
+    const static int LogLevelError = 0;
+    const static int LogLevelWarning = 1;
+    const static int LogLevelInfo = 2;
+private:
+    int m_LogLevel = LogLevelInfo;
+public:
+    void SetLevel(int level)
+    {
+        m_LogLevel = level;
+    }
+    void Warn(const char* msg) const {
+        if (m_LogLevel >= LogLevelWarning){
+            std::cout <<"[WARNING]: " << msg << std::endl;
+        }
+    }
+    void Error(const char* msg) const {
+        if (m_LogLevel >= LogLevelError) {
+            std::cout <<"[ERROR]: " << msg << std::endl;
+        }
+    }
+    void Info(const char* msg) const
+    {
+        if (m_LogLevel >=LogLevelInfo) {
+            std::cout <<"[INFO]: " << msg << std::endl;
+        }
+    }
+};
+
+int hello(int x, int y) {
+  return x+y;
+};
 
 int main() {
-    // char a;//1byte
-    // short b;//2bytes
-    // int c;//4bytes
-    // unsigned int d;//4bytes
-    // long e;//8bytes
-    // long long f;//8bytes
-    // float g=5.5f;//4bytes
-    // double h=5.2;//8bytes
-    // char* buffer = new char[8];//在堆上申请了8bytes内存
-
-  const Log log;
-    //log.SetLevel(log.LogLevelError);
-    log.Warn("Hello!");
-    log.Error("Hello!");
-    log.Info("Hello!");
-    std::cin.get() ;
-    return 0 ;
+  constexpr Log log;
+  int x=0;x++;
+  //log.SetLevel(log.LogLevelError);
+  log.Warn("Hello!");
+  log.Error("Hello!");
+  log.Info("Hello!");
+  std::cout << x << std::endl;
+  std::cin.get() ;
+  return 0 ;
 }
