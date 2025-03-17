af5d49bec068455493f3cce1b5d399b407a60c44
Yanggan <376139949@qq.com>
2025-03-17 23:32:02 +0800

    some modification

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 
 class Entity {
 public:
-  float x,y;
-  //构造函数
-  Entity() {
-    x = 0.0f;y = 0.0f;
-    std::cout << "Created Entity" << std::endl;
-  }
-  //构造函数重载
-  Entity(float a, float b) {
-    x = a;y = b;
-  }
-  //Destructor析构函数
-  ~Entity() {
-    std::cout << "Destroyed Entity" << std::endl;
-  }
+  float x=0,y=0;
+  // 构造函数
+   Entity() {
+     x = 0.0f;y = 0.0f;
+     std::cout << "Created Entity" << std::endl;
+   }
+   //构造函数重载
+   Entity(float a, float b) {
+     x = a;y = b;
+   }
+   //Destructor析构函数
+   ~Entity() {
+     std::cout << "Destroyed Entity" << std::endl;
+   }
 
   void Print() const {
     std::cout << x <<","<< y << std::endl;
@@ -23,7 +23,7 @@ public:
 };
 
 void Function() {
-  Entity e;
+  const Entity e;
   e.Print();
 }
 
