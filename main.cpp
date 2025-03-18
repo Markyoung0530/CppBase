72784150504ff60c65f2d495e635949f2a4fabc0
Yanggan <376139949@qq.com>
2025-03-18 12:08:37 +0800

    Practise about inherit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-class Entity {
+class Entity1 {
 public:
   float x=0,y=0;
   // 构造函数
-   Entity() {
+   Entity1() {
      x = 0.0f;y = 0.0f;
      std::cout << "Created Entity" << std::endl;
    }
    //构造函数重载
-   Entity(float a, float b) {
+   Entity1(float a, float b) {
      x = a;y = b;
    }
    //Destructor析构函数
-   ~Entity() {
+   ~Entity1() {
      std::cout << "Destroyed Entity" << std::endl;
    }
 
@@ -23,13 +23,46 @@ public:
 };
 
 void Function() {
-  const Entity e;
+  const Entity1 e;
   e.Print();
 }
 
+//---------------------------------------------------
+//Practise about inherit
+class Entity {
+public:
+  float X,Y;
+
+  Entity() {//构造函数
+    X = 0.0f;Y = 0.0f;
+  }
+
+  void Move(const float xa, const float ya) {
+    X+=xa;Y+=ya;
+  }
+};
+
+class Player : public Entity {
+  const char* Name;
+
+public:
+  Player() {
+    Name = "defaultName";
+  }
+
+  void PrintName() {
+    std::cout << Name << std::endl;
+  }
+};
+//---------------------------------------------------
+
+
 int main() {
-  std::cout << "main!!!" <<std::endl;
-  Function();
+  Player p;
+  p.PrintName();
+  std::cout << p.X <<","<< p.Y << std::endl;
+  p.Move(0.1f,1.0f);
+  std::cout << p.X <<","<< p.Y << std::endl;
   std::cin.get();
   return 0;
 }
