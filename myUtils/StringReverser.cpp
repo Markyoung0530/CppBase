2878ec122e4f59cbc4516f38b4ce58b7043d4720
Yanggan <376139949@qq.com>
2025-03-18 23:25:56 +0800

    重写reverse(),使用指针实现

--- myUtils/StringReverser.cpp
+++ myUtils/StringReverser.cpp
@@ -5,9 +5,15 @@
 
 std::string myUtils::StringReverser::reverse(const std::string& str) {
   std::string reversed = str;
-  int len = str.size();
-  for (int i = 0; i < len / 2; i++) {
-    std::swap(reversed[i],reversed[len - 1 -i]);
+  const size_t len = str.size();
+  char * ptr1 = &reversed[0];
+  char* ptr2 = &reversed[len-1];
+  while (ptr1 < ptr2) {
+    const char temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+    ptr1++;
+    ptr2--;
   }
   return reversed;
 };
